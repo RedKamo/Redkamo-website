f2183c20cc0175c791eb359110681ea13ef4d4ea
RedKamo <camiloandressuarez1@gmail.com>
2021-03-15 20:16:13 -0500

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
  const App = () => {
     return (
         <div>
+            <Header/>
             <h1>REDKAMO web portfoio</h1>
+            <Footer/>
         </div>
     )
 }
