20ff5e21d36db3c941c4471c04e4738e1bc61891
RedKamo <camiloandressuarez1@gmail.com>
2021-03-16 20:23:08 -0500

    banner layout and styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import {TiSocialLinkedinCircular, TiSocialTwitterCircular,TiSocialGithubCircular } from 'react-icons/ti';
+import '../../styles/Banner.scss';
 
 const Banner = () => {
   return (
     <div className="banner">
       <div className="banner__cover">
-      	<p>Hello,<br/> I´m <span className="banner__cover__name">Camilo Suarez</span>, <br/> Frontend developer and UI/UX enthusiast. </p>
+      	<p>Hello,<br/> I´m <span className="banner__cover__name">Camilo Suarez</span>,<br/> Frontend developer and UI/UX enthusiast. </p>
         <div className="banner__cover__social">
-          <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank"><TiSocialGithubCircular/></a>
+          <a href="https://github.com/RedKamo" target="_blank"><TiSocialGithubCircular/></a>
           <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank"><TiSocialLinkedinCircular/></a>
-          <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank"><TiSocialTwitterCircular/></a>
+          <a href="https://twitter.com/RedKamo_/" target="_blank"><TiSocialTwitterCircular/></a>
         </div>
       </div>
   	</div>
