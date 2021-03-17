3fb4d0a37ce13fc08cb157282e91388c0104600e
RedKamo <camiloandressuarez1@gmail.com>
2021-03-16 20:14:58 -0500

    add icons react

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import '../../styles/Banner.scss'
+import {TiSocialLinkedinCircular, TiSocialTwitterCircular,TiSocialGithubCircular } from 'react-icons/ti';
+
 const Banner = () => {
-    return (
-        <div className="banner">
-            <div className="banner__cover">
-                <p>Hello,<br/> I´m <span className="banner__cover__name">Camilo Suarez</span>, <br/> Frontend developer and UI/UX enthusiast. </p>
-            </div>
+  return (
+    <div className="banner">
+      <div className="banner__cover">
+      	<p>Hello,<br/> I´m <span className="banner__cover__name">Camilo Suarez</span>, <br/> Frontend developer and UI/UX enthusiast. </p>
+        <div className="banner__cover__social">
+          <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank"><TiSocialGithubCircular/></a>
+          <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank"><TiSocialLinkedinCircular/></a>
+          <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank"><TiSocialTwitterCircular/></a>
         </div>
-    )
+      </div>
+  	</div>
+  )
 }
 
 export default Banner
