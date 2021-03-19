45e96cbda276012708addbfd1776449072a85f79
RedKamo <camiloandressuarez1@gmail.com>
2021-03-18 21:02:21 -0500

    working on projects section --mobile version

--- src/components/CardProject/CardProject.jsx
+++ src/components/CardProject/CardProject.jsx
@@ -5,8 +5,8 @@ const CardProject = ({ projectName, projectImage, projectYear }) => {
   return (
     <div className="card">
       <img src={projectImage} alt="" />
-      <h2> {projectName} </h2>
-      <h3> {projectYear} </h3>
+      {/* <h2> {projectName} </h2>
+      <h3> {projectYear} </h3> */}
     </div>
   );
 };
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ProjectList from '../ProjectList/ProjectList';
 import '../../styles/Projects.scss';
-
+import alien from '../../assets/images/project1.svg';
 const Projects = () => {
   return (
     <div className="projects">
       <h1>Projects </h1>
-      <ProjectList />
+      <div className="projects__container">
+        <ProjectList />
+      </div>
+      <img src={alien} alt="" />
     </div>
   );
 };
