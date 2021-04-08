cd56ef25092929cf73a67a7fceaa94d384328194
RedKamo <camiloandressuarez1@gmail.com>
2021-04-08 17:48:16 -0500

    fix cover section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -14,22 +14,22 @@ const Banner = () => {
         <p>
           Hello,
           <br /> I´m <span className="banner__cover__name">Camilo Suarez</span>,
-          <br /> Frontend developer and UI/UX enthusiast.{' '}
+          <br /> Frontend developer and UI/UX enthusiast.
         </p>
-        <div className="banner__cover__social">
-          <a href="https://github.com/RedKamo" target="_blank">
-            <TiSocialGithubCircular />
-          </a>
-          <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank">
-            <TiSocialLinkedinCircular />
-          </a>
-          <a href="https://twitter.com/RedKamo_/" target="_blank">
-            <TiSocialTwitterCircular />
-          </a>
+        <div className="banner__cover__icon">
+          <img src={white_icon} alt="" />
         </div>
       </div>
-      <div className="banner__icon">
-        <img src={white_icon} alt="" />
+      <div className="banner__social">
+        <a href="https://github.com/RedKamo" target="_blank">
+          <TiSocialGithubCircular />
+        </a>
+        <a href="https://www.linkedin.com/in/camiloasuarez/" target="_blank">
+          <TiSocialLinkedinCircular />
+        </a>
+        <a href="https://twitter.com/RedKamo_/" target="_blank">
+          <TiSocialTwitterCircular />
+        </a>
       </div>
     </div>
   );
